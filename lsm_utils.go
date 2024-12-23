2af51122c8c938e29cef654ba32ee0f53fea298e
AshikBN <ashikbn1998@gmail.com>
2024-12-23 20:05:30 +0530

    implemented get and range query

--- lsm_utils.go
+++ lsm_utils.go
@@ -5,3 +5,12 @@ package swiftdb
 func isSSTableFile(filename string) bool {
 	return filename[:len(SSTableFilePrefix)] == SSTableFilePrefix
 }
+
+// check if the given entry is tombstone,return the value if it is not,
+// return nil if it is
+func handleValue(value *LSMEntry) ([]byte, error) {
+	if value.Command == Command_DELETE {
+		return nil, nil
+	}
+	return value.Value, nil
+}
--- merge_utils.go
+++ merge_utils.go
@@ -98,3 +98,50 @@ func mergeIterators(iterators []*SSTableIterator) []*LSMEntry {
 	return results
 
 }
+
+func mergeRanges(ranges [][]*LSMEntry) []KVPair {
+	minHeap := &mergeHeap{}
+	heap.Init(minHeap)
+
+	var results []KVPair
+
+	seen := skiplist.New(skiplist.String)
+
+	for i, entries := range ranges {
+		if len(entries) > 0 {
+			heap.Push(minHeap, heapEntry{entry: entries[0], listIndex: i, idx: 0})
+		}
+	}
+
+	for minHeap.Len() > 0 {
+		minEntry := heap.Pop(minHeap).(heapEntry)
+
+		previousValue := seen.Get(minEntry.entry.Key)
+		if previousValue != nil {
+			if previousValue.Value.(heapEntry).entry.Timestamp < minEntry.entry.Timestamp {
+				seen.Set(minEntry.entry.Key, minEntry)
+			}
+		} else {
+			seen.Set(minEntry.entry.Key, minEntry)
+		}
+
+		if minEntry.idx < len(ranges[minEntry.listIndex]) {
+			nextEntry := ranges[minEntry.listIndex][minEntry.idx+1]
+			heap.Push(minHeap, heapEntry{entry: nextEntry, listIndex: minEntry.listIndex, idx: minEntry.idx + 1})
+		}
+	}
+
+	iter := seen.Front()
+	for iter != nil {
+		entry := iter.Value.(heapEntry).entry
+		if entry.Command == Command_DELETE {
+			iter = iter.Next()
+			continue
+		}
+
+		results = append(results, KVPair{Key: entry.Key, Value: entry.Value})
+		iter = iter.Next()
+	}
+
+	return results
+}
